1b404b49dccb71898e5ef1d40b50fe8a43750882
Nektro <nektromc@gmail.com>
2017-10-08 20:01:35 -0400

    fix node issue and npm version bump

--- mantle.js
+++ mantle.js
@@ -286,7 +286,7 @@
         }
     });
 
-    if (typeof module !== undefined && 'exports' in module) {
+    if (typeof module !== 'undefined' && 'exports' in module) {
         module.exports = mantle;
     }
     else {
